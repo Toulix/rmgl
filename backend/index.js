efa009b6c310a2e9e17023fc4da363ec6d6783b2
Patrick André <tolojanaharypatrickandre@gmail.com>
2022-07-22 16:00:20 +0300

    setup (backend): integrate graphql with sample data for now

--- backend/index.js
+++ backend/index.js
@@ -1,10 +1,49 @@
 const express = require('express');
-const app = express();
+const { ApolloServer, gql } = require('apollo-server-express');
 
+const app = express();
 const port = process.env.PORT || 4000;
 
-app.get('/', (req, res) => res.send('Hello World'));
+let warriors = [
+ { id: '1', name: 'Merlin Gaulois', hp: 100, mp: 30, st: 40  },
+ { id: '1', name: 'Jacques Roumain', hp: 100, mp: 30, st: 60  },
+];
 
-app.listen(port, () =>
-console.log(`Server running at http://localhost:${port}`)
-);
+// Construct a schema, using GraphQL's schema language
+const typeDefs = gql`
+    type Warrior {
+        id: ID!
+        name: String!
+        hp: Int!
+        mp: Int!
+        st: Int!
+    }
+
+    type Query {
+        warriors: [Warrior!]!
+    }
+`;
+
+// Provide resolver functions for our schema fields
+const resolvers = {
+    Query: {
+        warriors: () => warriors
+    }
+};
+
+async function startApolloServer(typeDefs, resolvers){
+    
+    // Apollo Server setup
+    const server = new ApolloServer({typeDefs, resolvers})
+
+    await server.start();
+
+    // Apply the Apollo GraphQL middleware and set the path to /graphql
+    server.applyMiddleware({app, path: '/graphql'});
+    
+    app.listen({port}, () => {
+    console.log(`GraphQL Server running at http://localhost:${port}${server.graphqlPath}`);
+})
+}
+
+startApolloServer(typeDefs, resolvers);
