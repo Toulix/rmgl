960e9bc445d93c3aa44caff8c259e8b952cca6b1
Patrick André <tolojanaharypatrickandre@gmail.com>
2022-07-23 13:40:26 +0300

    setup (backend): add mongodb connexion

--- backend/index.js
+++ backend/index.js
@@ -1,33 +1,76 @@
 const express = require('express');
 const { ApolloServer, gql } = require('apollo-server-express');
-
 const app = express();
+
+const db = require("./db");
+
 const port = process.env.PORT || 4000;
 
+db.connect().then(() => {
+    console.log("Connected to MongoDB: " + db.url);
+  });  
+
 let warriors = [
- { id: '1', name: 'Merlin Gaulois', hp: 100, mp: 30, st: 40  },
- { id: '1', name: 'Jacques Roumain', hp: 100, mp: 30, st: 60  },
+ { id: '1', name: 'Merlin Gaulois', hp: 100, mp: 30, st: 40, type: 'Gaulois'  },
+ { id: '2', name: 'Jacques Roumain', hp: 100, mp: 30, st: 60, type: 'Roumain'  },
 ];
 
 // Construct a schema, using GraphQL's schema language
 const typeDefs = gql`
+
     type Warrior {
         id: ID!
         name: String!
         hp: Int!
         mp: Int!
         st: Int!
+        type: String!
+        creator: String!
+    }
+
+    input WarriorInput {
+        name: String!
+        hp: Int!
+        mp: Int!
+        st: Int!
+        type: String!
+        creator: String!
     }
 
     type Query {
         warriors: [Warrior!]!
+        getWarrior(id: ID!): Warrior!
+    }
+
+    type Mutation {
+        createWarrior(warriorInput: WarriorInput!): Warrior!
     }
 `;
 
 // Provide resolver functions for our schema fields
 const resolvers = {
+
     Query: {
-        warriors: () => warriors
+        warriors: () => warriors,
+        getWarrior: (parent,args) => warriors.find(warrior => warrior.id == args.id)
+    },
+
+    Mutation: {
+        createWarrior: (parent, args) => {
+            let warrior = {
+                id: warriors.length + 1,
+                name: args.name,
+                hp: args.hp,
+                mp: args.mp,
+                st: args.st,
+                type: args.type,
+                creator: args.creator
+            };
+
+            warriors.push(warrior);
+
+            return warrior;
+        }
     }
 };
 
