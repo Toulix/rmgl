892dc0c07f7be1d7128d0855f4ed77b3b5975b00
Toulix <tolojanaharypatrickandre@gmail.com>
2022-07-30 21:23:24 +0300

    feat (backend): create account for users

--- backend/resolvers/mutation.js
+++ backend/resolvers/mutation.js
@@ -1,3 +1,12 @@
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
+const {
+        AuthenticationError,
+        ForbiddenError
+        } = require('apollo-server-express');
+
+const jwt_secret = process.env.JWT_SECRET || 'warrior';
+
 module.exports = {
         // createWarrior: async (parent, args, { models }) => {
 
@@ -109,4 +118,26 @@ module.exports = {
                         });
         }
      },
+
+     signUp: async (parent, { username, email, password }, { models }) => {
+        // normalize email address
+        email = email.trim().toLowerCase();
+        // hash the password
+        const hashed = await bcrypt.hash(password, 10);
+
+        try {
+           const user = await models.User.create({
+                username,
+                email,
+                password: hashed
+                });
+        // create and return the json web token
+                return jwt.sign({ id: user._id }, jwt_secret);
+        } catch (err) {
+
+        console.log(err);
+        // if there's a problem creating the account, throw an error
+           throw new Error('Error creating account');
+        }
+        },
 }
