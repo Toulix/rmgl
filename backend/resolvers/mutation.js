2edadf7c2a31a2188134c3caa98c58c90a685b55
Toulix <tolojanaharypatrickandre@gmail.com>
2022-07-30 21:21:29 +0300

    feat (backend): add the ability to update warriors

--- backend/resolvers/mutation.js
+++ backend/resolvers/mutation.js
@@ -1,16 +1,112 @@
 module.exports = {
-        createWarrior: async (parent, args, { models }) => {
+        // createWarrior: async (parent, args, { models }) => {
 
-        const { name, hp, mp, st, type, creator } = args;
+        // const { name, hp, mp, st, type, creator } = args;
 
-        return await models.Warrior
-                           .create({
-                                name,
-                                hp,
-                                mp,
-                                st,
-                                type,
-                                creator                                      
-                            });
+        // return await models.Warrior
+        //                    .create({
+        //                         name,
+        //                         hp,
+        //                         mp,
+        //                         st,
+        //                         type,
+        //                         creator                                      
+        //                     });
+        // },
+
+//      * Roumain
+//              * Frapper (button)
+//                      * Action: HP (Gaulois) = HP (Gaulois) - ST (Romain)
+
+        hitGauloisWarrior: async (parent, {idRoumain, idGaulois }, { models }) => {
+        
+           const roumain =  await models.Warrior.find({ _id: idRoumain, type: 'Roumain' });
+                
+           if(roumain) {
+                return await models.Warrior.findOneAndUpdate(
+                        {
+                          _id: idGaulois,
+                        },
+                        {
+                         $set: {
+                           hp: hp - roumain.st
+                          }
+                        },
+                        {
+                         new: true
+                        });
+           }
+        },
+
+//      * Roumain
+//              * Lancer sort (button)
+//                      * Action: HP (Gaulois) = HP (Gaulois) - MP (Romain)
+
+        curseGauloisWarrior: async (parent, {idRoumain, idGaulois }, { models }) => {
+        
+        const roumain =  await models.Warrior.find({ _id: idRoumain, type: 'Roumain' });
+             
+        if(roumain) {
+             return await models.Warrior.findOneAndUpdate(
+                     {
+                       _id: idGaulois,
+                     },
+                     {
+                      $set: {
+                        hp: hp - roumain.mp
+                       }
+                     },
+                     {
+                      new: true
+                     });
         }
+     },
+
+//      Gaulois:
+//      * Frapper (button)
+//              * Action: HP (Romain) = HP (Romain) - ST (Gaulois)
+
+       hitRomainWarrior:  async (parent, {idGaulois, idRomain }, { models }) => {
+        
+        const gaulois =  await models.Warrior.find({ _id: idGaulois, type: 'Gaulois' });
+             
+        if(gaulois) {
+             return await models.Warrior.findOneAndUpdate(
+                     {
+                       _id: idRomain,
+                     },
+                     {
+                      $set: {
+                        hp: hp - gaulois.st
+                       }
+                     },
+                     {
+                      new: true
+                     });
+        }
+     },
+
+//      Gaulois:
+//      * Lancer sort (button)
+//              * Action: HP (Romain) = HP (Romain) - MP (Gaulois)
+
+        curseRomainWarrior: async (parent, {idGaulois, idRomain }, { models }) => {
+        
+                const gaulois =  await models.Warrior.find({ _id: idGaulois, type: 'Gaulois' });
+                
+                if(gaulois) {
+                return await models.Warrior.findOneAndUpdate(
+                        {
+                          _id: idRomain,
+                        },
+                        {
+                        $set: {
+                          hp: hp - gaulois.mp
+                        }
+                        },
+                        {
+                          new: true
+                        });
+        }
+     },
 }
--- backend/schema.js
+++ backend/schema.js
@@ -1,6 +1,8 @@
 const { gql } = require('apollo-server-express');
 
 // Construct a schema, using GraphQL's schema language
+// createWarrior(name: String!, hp: Int!, mp: Int!, st: Int!, type: String!, creator: User!): Warrior!
+
 module.exports = gql`
     type Warrior {
         id: ID!
@@ -9,16 +11,14 @@ module.exports = gql`
         mp: Int!
         st: Int!
         type: String!
-        creator: String!
+        creator: User!
     }
 
-    input WarriorInput {
-        name: String!
-        hp: Int!
-        mp: Int!
-        st: Int!
-        type: String!
-        creator: String!
+    type User {
+        id: ID!
+        username: String!
+        email: String!
+        warriors: [Warrior!]!
     }
 
     type Query {
@@ -27,11 +27,11 @@ module.exports = gql`
     }
 
     type Mutation {
-        createWarrior(name: String!
-                    hp: Int!
-                    mp: Int!
-                    st: Int!
-                    type: String!
-                    creator: String!): Warrior!
+        hitGauloisWarrior(idRoumain: ID!, idGaulois: ID!): Warrior!
+        hitRomainWarrior(idGaulois: ID!, idRoumain: ID!): Warrior!
+        curseGauloisWarrior(idRoumain: ID!, idGaulois: ID!): Warrior!
+        curseRomainWarrior(idGaulois: ID!, idRoumain: ID!): Warrior!
+        signUp(username: String!, email: String!, password: String!): String!
+        signIn(email: String, password: String!): String!
     }
 `;
